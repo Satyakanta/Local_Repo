da529aa4b1e5710feb20919cb800aa629dbbceed
GIT-PRAC <satyakantapatra18@gmail.com>
2026-02-02 19:39:55 +0530

    intial files

---
+++ code.java
@@ -0,0 +1,16 @@
+package Local_Repo;
+import java.util.*;
+
+
+public class code {
+    public static void main(String[] args) {
+        int a , b  , mul;
+        Scanner sc = new Scanner(System.in);
+         System.out.print("Enter the Num 1:- ");
+         a = sc.nextInt();
+         System.out.print("Enter the Num 2:- ");
+         b = sc.nextInt();
+         mul = a*b;
+         System.out.println("Your Multiplication is :- "+mul);
+    }
+}
